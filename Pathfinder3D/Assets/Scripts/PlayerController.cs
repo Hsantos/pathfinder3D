b376f193c7c9f665eeb6ebe3f741c23c742cda5d
Hsantos <igor.hos@gmail.com>
2018-12-15 18:15:05 -0200

    dev

---
+++ Pathfinder3D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+	void Update ()
+	{
+	    if (agent == null) return;
+
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(ray, out hit))
+	        {
+                //move our agent
+	            agent.SetDestination(hit.point);
+
+	        }
+
+	    }
+	}
+}
